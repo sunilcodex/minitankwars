024215e4504ef63794639f6ddbf40e71091ebed4
1Brian.Charles <1Brian.Charles@cfc77d08-f990-11de-8cdf-e97ad7d0f28a>
2010-01-09 21:20:07 +0000

    -First content commit

---
+++ trunk/gamemode/init.lua
@@ -0,0 +1,43 @@
+/*
+MiniTank Wars
+Copyright (c) 2010 BMCha
+This gamemode is licenced under the MIT License, reproduced in shared.lua
+*/
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include( "shared.lua" )
+
+// This is called every second to see if we can end the round
+function GM:CheckRoundEnd()
+ 
+	if ( !GAMEMODE:InRound() ) then return end 
+ 
+	for k,v in pairs( team.GetPlayers( TEAM_HUMAN ) ) do
+ 
+		if v:Frags() >= 10 then
+ 
+		      GAMEMODE:RoundEndWithResult( v )
+ 
+                end
+ 
+        end
+ 
+end
+ 
+// This is called after a player wins in a free for all
+function GM:OnRoundWinner( ply, resulttext )
+ 
+	ply:AddScore( 1 ) // Let's pretend we have AddScore for brevity's sake
+ 
+end
+ 
+// Called when the round ends
+function GM:OnRoundEnd( num )
+ 
+       for k,v in pairs( team.GetPlayers( TEAM_HUMAN ) ) do
+             v:SetFrags( 0 ) // Reset their frags for next round
+       end
+ 
+end
